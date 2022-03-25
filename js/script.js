540abb5864ca4dc9e4b1c171eeb91f3c9bdf7300
bordedelcielo <christopher.thrutchley@gmail.com>
2022-03-24 19:04:25 -0500

    querySelector with jQuery

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log(document.querySelector("img"));
+
+jQuery( window ).on( "load", function() {
+    console.log( "document loaded" );
+    console.log(document.querySelector("img"));
+} );
