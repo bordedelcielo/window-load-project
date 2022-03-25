f3acc0517608671c17a3adbba23228d06f9d0307
bordedelcielo <christopher.thrutchley@gmail.com>
2022-03-24 23:50:25 -0500

    added width query

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ console.log(document.querySelector("img"));
 jQuery( window ).on( "load", function() {
     console.log( "document loaded" );
     console.log(document.querySelector("img"));
+    console.log(document.querySelector("img").clientWidth);
 } );
